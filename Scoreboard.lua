9d897e2ae5a431499c57aa81a7066f61d2b7ce4f
T-MSD <dias.tiago03@gmail.com>
2023-09-25 18:02:59 +0100

    finished scoreboard

--- Scoreboard.lua
+++ Scoreboard.lua
@@ -7,11 +7,18 @@ function Scoreboard()
 
     draw = function (self, buttons, score, highscore)
       if self.drawable then
-        love.graphics.print("" .. score, 100, 100)
-        love.graphics.print("" .. highscore, 100, 200)
+        local font = love.graphics.newFont(70)
+        local black = {0, 0, 0}
+        love.graphics.setFont(font)
+
         love.graphics.draw(self.image, 810, 290)
         buttons.lostExit:draw(2900, 2340, 0.3)
         buttons.replay:draw(3300, 2340, 0.3)
+
+        local txtScore = "".. score
+        local txtHighscore = "" .. highscore
+        love.graphics.print({black, txtScore}, 960 - font:getWidth(txtScore)/2, 480)
+        love.graphics.print({black, txtHighscore}, 960 - font:getWidth(txtHighscore)/2, 610)
       end
     end
   }
